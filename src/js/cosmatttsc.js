579c96aec8bf06d9734e59a4e368d7f751e0c579
Adnan Kamili <adnan.kamili@pearson.com>
2017-07-31 10:59:05 +0530

    updated bootstrap slider

--- src/js/cosmatttsc.js
+++ src/js/cosmatttsc.js
@@ -31,8 +31,8 @@
 // 8:23 22/06/2017
 define([
     'css!../css/cosmatttsc.css', //Custom styles of the engine (applied over bootstrap & front-end-core)
-    '../../bower_components/bootstrap-slider/bootstrap-slider.js',
-    'css!../../bower_components/bootstrap-slider/slider.css',
+    '../../bower_components/seiyria-bootstrap-slider/dist/bootstrap-slider.min.js',
+    'css!../../bower_components/seiyria-bootstrap-slider/dist/css/bootstrap-slider.min.css',
     '../../bower_components/flot/jquery.flot.js',
     '../../bower_components/flot/jquery.flot.resize.js',
     '../../bower_components/flot-axislabels/jquery.flot.axislabels.js',
